ce3e3479201d6934b6b9ec5fef78c072ce31bad6
Metajunkie <metajunkie@gmail.com>
2020-02-15 17:17:02 -0500

    Initial Commit

---
+++ CoreDataToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataToDo
+//
+//  Created by kewal on 2/15/20.
+//  Copyright © 2020 DigiDeeds. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
